ba66cf2d3a291fd939914bdd72682c1a33d0a509
Kyle R Wenholz <kyle@krwenholz.com>
2019-08-25 15:16:17 -0700

    Add policy for S3 bucket

--- modules/storage/storage.tf
+++ modules/storage/storage.tf
@@ -27,12 +27,26 @@ resource "aws_s3_bucket" "buckets" {
 
   bucket = var.configurations[count.index].bucket_name
   acl    = "public-read"
-  # TODO(kyle): Do I need this?
-  #policy = "${file("policy.json")}"
+
+  policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadForGetBucketObjects",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${var.configurations[count.index].bucket_name}/*"
+    }
+  ]
+}
+EOF
 
   website {
     index_document = "index.html"
-    error_document = "error.html"
   }
 
   versioning {
